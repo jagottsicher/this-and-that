eab0a7069a0b7f96752eba4ca4e2ed6dcd6ad775
jagottsicher <jagottsicher@gmx.net>
2020-11-11 21:13:48 +0800

    add empty edges/bounderies and finalized code (postponedcheck for dynnamic balance

--- game-of-life_pointer-and-functions/main.c
+++ game-of-life_pointer-and-functions/main.c
@@ -9,7 +9,7 @@ int height = 0;
 int generationCounter = 0;
 bool goOn = true;
 
-bool buildArray(bool *ptr_arrayA, bool *ptr_arrayB, bool *ptr_arrayC);
+bool buildArray(bool *ptr_arrayA, bool *ptr_arrayB);
 
 int main()
 {
@@ -21,41 +21,58 @@ srand(time(NULL));
 clearScreen();
 
 // get screen size
-width = checkAndSetConsoleDimensions('x');
-height = checkAndSetConsoleDimensions('y');
+width = checkAndSetConsoleDimensions('x')+2;
+height = checkAndSetConsoleDimensions('y')+2;
 
 // printf("%d/%d", width, height);
 
 // allocate memory for three pointers to arrays of size bool
-bool *ptr_array1 = malloc((width * height) * sizeof(bool));
-bool *ptr_array2 = malloc((width * height) * sizeof(bool));
-bool *ptr_array3 = malloc((width * height) * sizeof(bool));
+bool *ptr_array1 = malloc(((width) * (height)) * sizeof(bool));
+bool *ptr_array2 = malloc(((width) * (height)) * sizeof(bool));
+// bool *ptr_array3 = malloc(((width) * (height)) * sizeof(bool));
 
  // Arrays mit 0 füllen.
-for (int i = 0; i < height; i++)
+for (int i = 0; i < (height); i++)
     {
-    for (int j = 0; j < width; j++)
+    for (int j = 0; j < (width); j++)
         {
-            (*(ptr_array1+((i * width) + j))) = false;
-            (*(ptr_array2+((i * width) + j))) = false;
-            (*(ptr_array3+((i * width) + j))) = false;
+            (*(ptr_array1+((i * (width)) + j))) = false;
+            (*(ptr_array2+((i * (width)) + j))) = false;
+//            (*(ptr_array3+((i * (width)) + j))) = false;
         }
     }
 
 // Array1 in zwei geschachtelten for Schleifen mit zufällig mit 0 und 1 füllen
-for (int i = 0; i < height; i++)
+for (int i = 0; i < (height); i++)
     {
-        for (int j = 0; j < width; j++)
+    for (int j = 0; j < (width); j++)
         {
-            if (returnRandom(0,1) == 1)
-            {
-                (*(ptr_array1+((i * width) + j))) = true;
-            }
-            else
-            {
-                (*(ptr_array1+((i * width) + j))) = false;
-            }
-          // array1[j][i] = returnRandom(0,1);
+           if (returnRandom(0,1) == 1)
+           {
+            (*(ptr_array1+((i * width) + j))) = true;
+           }
+           else
+           {
+            (*(ptr_array1+((i * width) + j))) = false;
+           }
+        }
+    }
+
+// upper lower bounds zero
+for (int i = 0; i < (height); i = i + height - 1)
+    {
+    for (int j = 0; j < (width); j++)
+        {
+            (*(ptr_array1+((i * width) + j))) = false;
+        }
+    }
+
+// left right bounds zero
+for (int i = 0; i < (height); i++)
+    {
+    for (int j = 0; j < (width); j = j + width - 1)
+        {
+            (*(ptr_array1+((i * width) + j))) = false;
         }
     }
 
@@ -68,9 +85,9 @@ if (generationCounter % 2 == 0)
 //    goto 1/1
 printf("\33[%d;%dH", 1, 1);
 // printout array1
-for (int i = 0; i < height; i++)
+for (int i = 1; i < (height-1); i++)
     {
-    for (int j = 0; j < width; j++)
+    for (int j = 1; j < (width-1); j++)
         {
             if ((*(ptr_array1+((i * width) + j))) == true)
             {
@@ -83,17 +100,17 @@ for (int i = 0; i < height; i++)
         }
     // printf("\n");
     }
-printf("\33[%d;%dH%d", height-2, width-10, generationCounter);
-goOn = buildArray(ptr_array1, ptr_array2, ptr_array3);
+printf("\33[%d;%dH%d", height-4, width-10, generationCounter);
+goOn = buildArray(ptr_array1, ptr_array2);
 }
 else
 {
 //    goto 1/1
 printf("\33[%d;%dH", 1, 1);
 // printout array2
-for (int i = 0; i < height; i++)
+for (int i = 1; i < (height-1); i++)
     {
-    for (int j = 0; j < width; j++)
+    for (int j = 1; j < (width-1); j++)
         {
             if ((*(ptr_array2+((i * width) + j))) == true)
             {
@@ -106,44 +123,31 @@ for (int i = 0; i < height; i++)
         }
     // printf("\n");
     }
-printf("\33[%d;%dH%d", height-2, width-10, generationCounter);
-goOn = buildArray(ptr_array2, ptr_array1, ptr_array3);
+printf("\33[%d;%dH%d", height-4, width-10, generationCounter);
+goOn = buildArray(ptr_array2, ptr_array1);
 }
-msleep(100);
+msleep((generationCounter/5));
 generationCounter++;
+if (generationCounter == 1501)
+    goOn = false;
 }
 while (goOn == true);
 
 free (ptr_array1);
 free (ptr_array2);
-free (ptr_array3);
+// free (ptr_array3);
 }
 
-bool buildArray(bool *ptr_arrayA, bool *ptr_arrayB, bool *ptr_arrayC)
+bool buildArray(bool *ptr_arrayA, bool *ptr_arrayB)
 {
-//    iterate through array
-//    copy arrayA to arrayC
-
-// check if identical if yes return false
-//for (int i = 0; i < height; i++)
-//    {
-//    for (int j = 0; j < width; j++)
-//        {
-//            if (*(ptr_arrayC+((i * width) + j)) == *(ptr_arrayA+((i * width) + j)))
-//                return false;
-//        }
-//    }
-
-for (int i = 0; i < height; i++)
+for (int i = 1; i < (height-1); i++)
     {
-    for (int j = 0; j < width; j++)
+    for (int j = 1; j < (width-1); j++)
         {
-            *(ptr_arrayC+((i * width) + j)) = *(ptr_arrayA+((i * width) + j));
-            // test only needed instead is
-            //    fill arrayB
-            // *(ptr_arrayB+((i * width) + j)) = true;
+//            *(ptr_arrayC+((i * width) + j)) = *(ptr_arrayA+((i * width) + j));
+
             int living_cells_around = 0;
-            // number of neighbors
+
             if (*(ptr_arrayA+(((i-1) * width) + (j-1))) == true)
                 living_cells_around++;
             if (*(ptr_arrayA+(((i-1) * width) + j)) == true)
@@ -160,6 +164,7 @@ for (int i = 0; i < height; i++)
                 living_cells_around++;
             if (*(ptr_arrayA+(((i+1) * width) + (j+1))) == true)
                 living_cells_around++;
+
             // conclusion and actions
             if (*(ptr_arrayA+((i * width) + j)) == true)
             {
@@ -194,18 +199,3 @@ for (int i = 0; i < height; i++)
 
 return true;
 }
-
-
-//bool function(array pointerA, array pointerB)
-//    iterate through array
-//    copy arrayB to arrayC
-//    fill arrayB
-//    compare arrayB = arrayC
-//        loop
-//            if any object != returncode = 0 break else returncode = 1
-//
-//return returncode;
-//*/
-
-
-
