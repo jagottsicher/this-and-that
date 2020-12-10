63c8d9b749a96ede0fe4f82224a0566dfc0f5930
Jens Schendel <jagottsicher@gmx.net>
2020-12-10 16:32:48 +0800

    added minor changes

---
+++ ncurses-sandbox/15-04_dumpwin.c
@@ -0,0 +1,36 @@
+#include <ncurses.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    char word[7];
+    int x,w,r;
+
+    srandom((unsigned)time(NULL));
+    word[6] = '\0';
+    initscr();
+
+/* add some random 6-char words */
+    for(x=0;x<200;x++)
+    {
+        for(w=0;w<6;w++)
+            word[w] = (random() % 26) + 'a';
+        printw("%s\t",word);
+    }
+    addstr("\nPress Enter to dump the screen");
+    refresh();
+    getch();
+
+/* write the window */
+    r = scr_dump("dump.win");
+    if( r == ERR)
+        addstr("Error writing window");
+    else
+        addstr("File written; press Enter");
+    refresh();
+    getch();
+
+    endwin();
+    return(0);
+}
