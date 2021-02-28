5f0a22ae1aff3a858805029d4ad95ac3b5ee7293
jagottsicher <jagottsicher@gmx.net>
2021-02-28 20:58:26 +0800

    corrected pointer arithmetic in output and moved offsets for pointer in loops around

--- scrolling-with-structs/02_struct-nested.c
+++ scrolling-with-structs/02_struct-nested.c
@@ -19,23 +19,24 @@ struct Spots {
 };
 
 // internal funtions
-void outputTheStage (int x, int y, struct Spots *ptr_currentSpot);
-
+void outputTheStage (int x, int y, struct Spots *ptr_handlerSpot);
 
 int main() {
 //    struct Spots aRow[HEIGHT][WIDTH];
 struct Spots *aRow = malloc(DIMENSIONS * sizeof(struct Spots));
-// create a pointer as moving pointer
-struct Spots *ptr_currentSpot;
-ptr_currentSpot = aRow;
+
 // create a pointer as origin startpoint
 struct Spots *ptr_toOrigin;
 ptr_toOrigin = aRow;
+
+// create a pointer as moving pointer
+struct Spots *ptr_currentSpot;
+ptr_currentSpot = aRow;
+
 // j to go forward counting ABC...
 int j = 0;
 
 //	fill a all members line randomly and connect the end with the beginning
-
      for (int i = 0 ; i < (DIMENSIONS); i++) {
 		//fill each element with random number
 		aRow->terrain = randomNumber(65,90);
@@ -49,22 +50,26 @@ int j = 0;
 				aRow->right = aRow + 1;
 			}
 			else {
-				aRow->right = aRow - WIDTH;
+				// printf("%d goes to %d\n", i, i - (WIDTH - 1));
+				aRow->right = aRow - (WIDTH - 1);
 			}
 		// assign each element to it's left member except the first elements in each row that assign to last members
 		if (!(i % WIDTH == (0))) {
 				aRow->left = aRow - 1;
 			}
 			else {
-				aRow->left = aRow + WIDTH;
+				aRow->left = aRow + (WIDTH - 1);
+				// printf("%d to %d\n", i, (i + WIDTH - 1));
 			}
 
 		// all below pointers set except the last WIDTH ones
 		if (i < DIMENSIONS - WIDTH)
 			aRow->down = aRow + WIDTH;
 		// last WIDTH ones point to the very first WIDTH ones
-		if (i >= DIMENSIONS - WIDTH)
-			aRow->down = aRow - i + WIDTH;
+		if (i >= (DIMENSIONS - WIDTH))
+			aRow->down = aRow + WIDTH;
+
+			//aRow->down = aRow + (WIDTH - i); // THIS IS THE PROBLEM!!!!
 
 		// all upper pointers set except the first WIDTH ones
 		if (i >= WIDTH)
@@ -78,45 +83,94 @@ int j = 0;
 		}
 // go back to very first element
 // aRow = aRow - DIMENSIONS;
-aRow = ptr_toOrigin;
-msleep(5000);
+// aRow = ptr_toOrigin;
 
-//outputTheStage (0,0, ptr_currentSpot); // output without offset
-//outputTheStage (0,0, ptr_currentSpot->left); // output moving left OK
-//outputTheStage (0,0, ptr_currentSpot->up); // output moving up ?? PRÜFEN!!!
-//outputTheStage (0,0, ptr_currentSpot->right); // output moving right OK
-outputTheStage (0,0, ptr_currentSpot->down); // output moving down OK
+//msleep(5000);
+
+//moving 10 to the right
+ptr_currentSpot = ptr_toOrigin;
+
+//// Moving more than the width left and right, right and left
+//for (int x = 0 ; x < 300 ; x++) {
+//	ptr_currentSpot = ptr_currentSpot->left;
+//	outputTheStage (0,0, ptr_currentSpot);
+//	//msleep(100);
+//}
+//
+//for (int x = 0 ; x < 300 ; x++) {
+//	ptr_currentSpot = ptr_currentSpot->right;
+//	outputTheStage (0,0, ptr_currentSpot);
+//	//msleep(100);
+//}
+//
+//for (int x = 0 ; x < 300 ; x++) {
+//	ptr_currentSpot = ptr_currentSpot->right;
+//	outputTheStage (0,0, ptr_currentSpot);
+//	//msleep(100);
+//}
+//
+//for (int x = 0 ; x < 300 ; x++) {
+//	ptr_currentSpot = ptr_currentSpot->left;
+//	outputTheStage (0,0, ptr_currentSpot);
+//	//msleep(100);
+//}
+
+//
+// IF going too far down seg fault // CHECK THIS
+for (int x = 0 ; x < 23 ; x++) {
+	ptr_currentSpot = ptr_currentSpot->down;
+	outputTheStage (0,0, ptr_currentSpot);
+	//msleep(100);
+}
+
+for (int x = 0 ; x < 23 ; x++) {
+	ptr_currentSpot = ptr_currentSpot->up;
+	outputTheStage (0,0, ptr_currentSpot);
+	//msleep(100);
+}
+
+//#define OFFSET_Y (35)
+//#define OFFSET_X (100)
+//
+//for (int i = 0 ; i < OFFSET_X ; i++)
+//	ptr_currentSpot = ptr_currentSpot->right;
+//for (int i = 0 ; i < OFFSET_Y ; i++)
+//	ptr_currentSpot = ptr_currentSpot->down;
+
 
 
 
 // print struct size and world size
-printf("\n%d bytes/struct and %d bytes/world\n", sizeof(struct Spots), DIMENSIONS * sizeof(struct Spots));
+// printf("\n%d bytes/struct and %d bytes/world\n", sizeof(struct Spots), DIMENSIONS * sizeof(struct Spots));
 // msleep(5000);
 
 
 // cleanup and free the memory
 //	free (aRow); // is this okay?
 //	free (ptr_currentSpot);
+//	free (ptr_handlerSpot);
 //	free (ptr_toOrigin);
 
     return 0;
 }
 
-void outputTheStage (int x, int y, struct Spots *ptr_currentSpot) {
+void outputTheStage (int x, int y, struct Spots *ptr_handlerSpot) {
+
 //	output of a complete screen
 	// clear screen
     clearScreen();
-	// pro height
+	// per times height
 	for (int j = 0 ; j < checkAndSetConsoleDimensions('y') ; j++) {
 		// printout a row
 		for (int i = 0 ; i < checkAndSetConsoleDimensions('x') ; i++) {
-			putc(ptr_currentSpot->terrain, stdout);
-			ptr_currentSpot = ptr_currentSpot->right;
+			putc(ptr_handlerSpot->terrain, stdout);
+			ptr_handlerSpot = ptr_handlerSpot->right;
 			fflush(stdout);
 		}
 		// back to the beginning of the current row
-		ptr_currentSpot = ptr_currentSpot - checkAndSetConsoleDimensions('x');
-		// and set the pointer to the element below
-		ptr_currentSpot = ptr_currentSpot->down;
+		for (int i = 0 ; i < checkAndSetConsoleDimensions('x') ; i++) {
+			ptr_handlerSpot = ptr_handlerSpot->left;
+		}		// and set the pointer to the element below
+		ptr_handlerSpot = ptr_handlerSpot->down;
 	} // end of full screen height
 }
