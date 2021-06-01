00a51b5117cb53dd7ad00b39b1a616b4758727ae
jagottsicher <jagottsicher@gmx.net>
2021-06-02 07:19:02 +0800

    updated

---
+++ ncurses-sandbox/04-11_unicode.c
@@ -0,0 +1,26 @@
+#define _XOPEN_SOURCE_EXTENDED 1
+#include <ncurses.h>
+#include <locale.h>
+
+int main()
+{
+    cchar_t heart = {
+        A_NORMAL,
+        L"\u2665"
+    };
+
+    /* activate Unicode character set */
+    setlocale(LC_CTYPE,"en_US.UTF-8");
+
+    initscr();
+
+    addstr("I ");
+    add_wch(&heart); // DOES THE HELL NOT WORK!
+    // addstr("♥"); // DOES NOT WORK EITHER!
+    addstr(" Ncurses");
+    refresh();
+    getch();
+
+    endwin();
+    return(0);
+}
