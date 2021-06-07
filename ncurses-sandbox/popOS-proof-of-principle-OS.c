24f7af887b6a5a515d33270bd07c677da5d729c8
jagottsicher <jagottsicher@gmx.net>
2021-06-07 16:34:27 +0800

    added some stuff about ncurses sidebar

--- ncurses-sandbox/popOS-proof-of-principle-OS.c
+++ ncurses-sandbox/popOS-proof-of-principle-OS.c
@@ -1,5 +1,9 @@
 #include <ncurses.h>
 
+#define MINSIDEBARWIDTH 30
+#define SIDEBARDIVIDER 7
+
+
 int main() {
 	// init
     initscr(); // init ncurses
@@ -8,6 +12,10 @@ int main() {
     curs_set(0); // no visible cursor
 
     WINDOW *sidebar_opener;
+    WINDOW *sidebar;
+    int sidebaropen = 0;
+    char input;
+    int sidebarwidth = MINSIDEBARWIDTH;
 
     /* colors */
     start_color();
@@ -24,7 +32,53 @@ int main() {
 		wmove(sidebar_opener,1,1);
 		waddch(sidebar_opener,A_ALTCHARSET | ACS_PLMINUS);
 		wrefresh(sidebar_opener);
-		wgetch(sidebar_opener);
+	}
+
+	while (1) {
+		input = wgetch(sidebar_opener);
+		sidebarwidth = ((COLS / SIDEBARDIVIDER) > MINSIDEBARWIDTH) ? MINSIDEBARWIDTH : (COLS / SIDEBARDIVIDER);
+
+		if (input == '+') {
+			if (sidebaropen == 0) {
+				mvwin(sidebar_opener,0,3);
+				sidebar = newwin(COLS,3,0,0);
+				wbkgd(sidebar, COLOR_PAIR(1));
+				sidebaropen = 1;
+				if (sidebar == NULL) {
+					printf("Opening the window epic-failed");
+					break;
+				} else {
+					mvwaddstr(sidebar,0,0,"\n\n\n\n 1\n\n 2\n\n 3\n\n .\n\n X\n");
+					wrefresh(sidebar);
+					wrefresh(sidebar_opener);
+				}
+			} else
+					if (sidebaropen == 1) {
+						mvwin(sidebar_opener,0,sidebarwidth);
+						wresize(sidebar,COLS,sidebarwidth);
+						wbkgd(sidebar, COLOR_PAIR(1));
+						sidebaropen = 2;
+						if (sidebar == NULL) {
+							printf("Resizing the window epic-failed");
+							break;
+						} else {
+							mvwaddstr(sidebar,0,0,"\n\n\n\n Frist\n\n Second\n\n Third\n\n ...\n\n Xth\n");
+							wrefresh(sidebar);
+							wrefresh(sidebar_opener);
+						}
+					}
+				 else
+					if (sidebaropen == 2) {
+						delwin(sidebar);
+						touchwin(stdscr);
+						mvwin(sidebar_opener,0,0);
+						sidebaropen = 0;
+						refresh();
+						wrefresh(sidebar_opener);
+						}
+
+			}
+	input = NULL;
 	}
 
 	// and out
